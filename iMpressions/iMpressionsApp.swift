ac344cbb8ee922baec543ea388758dd650575b8c
Mello Birkan <mgbirkan@icloud.com>
2024-03-06 14:23:33 -0300

    Initial Commit

---
+++ iMpressions/iMpressionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMpressionsApp.swift
+//  iMpressions
+//
+//  Created by Marcello Gonzatto Birkan on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iMpressionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
